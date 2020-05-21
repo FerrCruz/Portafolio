6091d58d7c328da4dd58523b2d65917c442fef6c
FerrCruz <cfer3890@gmail.com>
2020-05-20 21:14:20 -0300

    Portafolio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function($){
+     var typed = new Typed('span.txt-rotate', {
+          strings: ["I'm a Web Developer Full Stack", "I'm BackEnd Developer", "I'm FrontEnd Developer"],
+          typeSpeed: 100,
+          backSpeed: 100,
+          fadeOut: false,
+          smartBackspace: true,
+          loop: true
+     });
+})(jQuery);
