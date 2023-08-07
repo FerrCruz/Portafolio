716cb3b04d7b708f51be60b4617f3ffa60385a77
FerrCruz <cfer3890@gmail.com>
2023-08-07 02:20:42 -0300

    Edicion y agregado de información y generador de codigo qr

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function($){
      var typed = new Typed('span.txt-rotate', {
-          strings: ["I'm a Web Developer Full Stack", "I'm BackEnd Developer", "I'm FrontEnd Developer"],
+          strings: ["I'm a Web Developer", "I'm BackEnd Developer", "I'm FrontEnd Developer"],
           typeSpeed: 100,
           backSpeed: 100,
           fadeOut: false,
