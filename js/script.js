96a9a0c6222c0ca39549f7b24855d82ca34ff70e
FerrCruz <cfer3890@gmail.com>
2023-08-08 17:46:30 -0300

    Proyecto punteria juego enlazado a QR

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 (function($){
      var typed = new Typed('span.txt-rotate', {
-          strings: ["I'm a Web Developer", "I'm BackEnd Developer", "I'm FrontEnd Developer"],
-          typeSpeed: 100,
-          backSpeed: 100,
+          strings: ["I'm a Web Developer", "I'm BackEnd Developer", "and FrontEnd Developer"],
+          typeSpeed: 50,
+          backSpeed: 75,
           fadeOut: false,
           smartBackspace: true,
           loop: true
