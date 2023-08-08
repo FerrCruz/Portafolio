231bb6cff6feac84e3767546c32262eb36fee3c9
FerrCruz <cfer3890@gmail.com>
2023-08-08 19:07:28 -0300

    Edicion de texto proyectos y modificacion de tiempo de escritura

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function($){
      var typed = new Typed('span.txt-rotate', {
-          strings: ["I'm a Web Developer", "I'm BackEnd Developer", "and FrontEnd Developer"],
+          strings: ["Soy Desarrollador BackEnd", "y Desarrollador FrontEnd",":)"],
           typeSpeed: 50,
           backSpeed: 75,
           fadeOut: false,
