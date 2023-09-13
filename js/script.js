626a6ad3b83140b7ba48b925d4a35f8cc3785683
FerrCruz <cfer3890@gmail.com>
2023-09-13 13:08:31 -0300

    Cambios necesarios

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function($){
      var typed = new Typed('span.txt-rotate', {
-          strings: ["Soy Desarrollador BackEnd", "y Desarrollador FrontEnd",":)"],
+          strings: ["Desarrollador BackEnd", "Desarrollador FrontEnd",":)"],
           typeSpeed: 50,
           backSpeed: 75,
           fadeOut: false,
